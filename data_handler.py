9cd85725e0cedbacd0f365e19fecfd245a8f6815
Pudding <1362061587@qq.com>
2024-10-23 17:21:59 +0800

    fix parse error (#8)
    修复部分字段（如店名和金额部分）使用了逗号，导致的错误分割

--- data_handler.py
+++ data_handler.py
@@ -1,37 +1,37 @@
+import csv
+
 def alipay_data(filepath):
     data = []
     with open(filepath, "r", encoding="gbk") as f:
-        lines = f.readlines()
+        reader = csv.reader(f)
         is_start = False
         jump_line = True
-        for line in lines:
+        for row in reader:
             if not is_start:
-                if "电子客户回单" in line:
+                if "电子客户回单" in row[0]:
                     is_start = True
                 continue
             if jump_line:
                 jump_line = False
                 continue
-            data_line = line.strip().split(",")
-            data.append(data_line)
+            data.append(row)
     return data
-
 
 # 微信导出字段：交易时间,交易类型,交易对方,商品,金额(元),支付方式,当前状态,备注
 def wechat_data(filepath):
     data = []
     with open(filepath, "r", encoding="utf-8") as f:
-        lines = f.readlines()
+        # 使用 csv.reader 来处理带引号和逗号的复杂字段
+        reader = csv.reader(f)
         is_start = False
         jump_line = True
-        for line in lines:
+        for row in reader:
             if not is_start:
-                if "微信支付账单明细列表" in line:
+                if "微信支付账单明细列表" in row[0]:
                     is_start = True
                 continue
             if jump_line:
                 jump_line = False
                 continue
-            data_line = line.strip().split(",")
-            data.append(data_line)
+            data.append(row)
     return data
