26be8715f12fd6a98c74158001e124dad4940373
edge_sky <1109467689@qq.com>
2024-10-23 17:23:39 +0800

    Revert "fix parse error (#8)"
    This reverts commit 9cd85725e0cedbacd0f365e19fecfd245a8f6815.

--- data_handler.py
+++ data_handler.py
@@ -1,37 +1,37 @@
-import csv
-
 def alipay_data(filepath):
     data = []
     with open(filepath, "r", encoding="gbk") as f:
-        reader = csv.reader(f)
+        lines = f.readlines()
         is_start = False
         jump_line = True
-        for row in reader:
+        for line in lines:
             if not is_start:
-                if "电子客户回单" in row[0]:
+                if "电子客户回单" in line:
                     is_start = True
                 continue
             if jump_line:
                 jump_line = False
                 continue
-            data.append(row)
+            data_line = line.strip().split(",")
+            data.append(data_line)
     return data
+
 
 # 微信导出字段：交易时间,交易类型,交易对方,商品,金额(元),支付方式,当前状态,备注
 def wechat_data(filepath):
     data = []
     with open(filepath, "r", encoding="utf-8") as f:
-        # 使用 csv.reader 来处理带引号和逗号的复杂字段
-        reader = csv.reader(f)
+        lines = f.readlines()
         is_start = False
         jump_line = True
-        for row in reader:
+        for line in lines:
             if not is_start:
-                if "微信支付账单明细列表" in row[0]:
+                if "微信支付账单明细列表" in line:
                     is_start = True
                 continue
             if jump_line:
                 jump_line = False
                 continue
-            data.append(row)
+            data_line = line.strip().split(",")
+            data.append(data_line)
     return data
