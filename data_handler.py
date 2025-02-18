f713c606dc699a3665a58552eba1401b01d76302
edge-sky <1109467689@qq.com>
2025-02-18 15:25:22 +0800

    对商户名中出现英文逗号的情况进行处理

--- data_handler.py
+++ data_handler.py
@@ -16,11 +16,16 @@ def alipay_data(filepath):
         if debug:
             print(reader)
         for row in reader:
+            # 对商户名中出现英文逗号的情况进行处理
+            while len(row) > 13:
+                row[2] += row[3]
+                row.pop(3)
             if not is_start:
                 # 支付宝可能存在空行，防止越界
                 for item in row:
                     if "电子客户回单" in item:
                         is_start = True
+                        break
                 continue
             # 跳过说明行
             if jump_line:
@@ -41,12 +46,16 @@ def wechat_data(filepath):
         if debug:
             print(reader)
         for row in reader:
-            print(row)
+            # 对商户名中出现英文逗号的情况进行处理
+            while len(row) > 11:
+                row[2] += row[3]
+                row.pop(3)
             if not is_start:
                 # 与支付宝统一格式
                 for item in row:
                     if "微信支付账单明细列表" in item:
                         is_start = True
+                        break
                 continue
             if jump_line:
                 jump_line = False
